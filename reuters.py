0e4cfb9a5edef1893309e0fa257f0a047adce9d3
yjp19871013 <yjp19871013@126.com>
2019-03-22 18:06:37 +0800

    添加项目文件

---
+++ reuters.py
@@ -0,0 +1,71 @@
+from keras.datasets import reuters
+import numpy as np
+from keras.utils import to_categorical
+from keras import models
+from keras import layers
+import matplotlib.pyplot as plt
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1
+    return results
+
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+one_hot_train_labels = to_categorical(train_labels)
+one_hot_test_labels = to_categorical(test_labels)
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+
+y_val = one_hot_train_labels[:1000]
+partial_y_train = one_hot_train_labels[1000:]
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=9,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+
+epochs = range(1, len(loss_values) + 1)
+
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+plt.clf()
+
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
+
+epochs = range(1, len(loss_values) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show() 
