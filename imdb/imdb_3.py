5a0afbe2eef01dfe3638d180935f543197cc7210
yjp19871013 <yjp19871013@126.com>
2019-04-08 16:34:03 +0800

    imdb_3.py添加tensorboard回调

--- imdb/imdb_3.py
+++ imdb/imdb_3.py
@@ -2,6 +2,7 @@ from keras.datasets import imdb
 from keras.preprocessing import sequence
 from keras.layers import Dense, Embedding, LSTM
 from keras import models
+from keras.callbacks import TensorBoard
 import matplotlib.pyplot as plt
 
 max_features = 10000
@@ -25,7 +26,17 @@ model.add(LSTM(32))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-history = model.fit(input_train, y_train, epochs=10, batch_size=128, validation_split=0.2)
+
+callbacks = [
+    TensorBoard(
+        log_dir='my_log_dir',
+        histogram_freq=1,
+        write_graph=True, 
+        write_images=True
+    )
+]
+
+history = model.fit(input_train, y_train, epochs=10, batch_size=128, validation_split=0.2, callbacks=callbacks)
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
